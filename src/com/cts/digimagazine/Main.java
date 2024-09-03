0f87addde25cb780464c2fb574cabba4c1d0ca95
Sharath-temp <1rn20is144.sharathkumarbs@gmail.com>
2024-09-03 14:31:30 +0545

    Created a project structure

--- src/com/cts/digimagazine/Main.java
+++ src/com/cts/digimagazine/Main.java
@@ -1,10 +1,10 @@
-package main;
+package com.cts.digimagazine;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello from main");
+
 	}
 
 }
---
+++ src/com/cts/digimagazine/model/Article.java
@@ -0,0 +1,5 @@
+package com.cts.digimagazine.model;
+
+public class Article {
+
+}
